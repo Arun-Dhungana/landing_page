a8b0e1606caa0e5ed429a947ff18fceabfe98110
Arun Dhungana <a.d.since03@gmail.com>
2024-06-08 11:33:11 +0545

    Initial setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,20 @@
+import { Campaigns } from "./(components)/campaigns";
+import { Carousel } from "./(components)/carousel";
+import { Contact } from "./(components)/contact";
+import { Latestresource } from "./(components)/latestresource";
+import { Image } from "./(components)/homeimage";
+import { News } from "./(components)/news";
+import { Whoweare } from "./(components)/who_we_are";
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <Image></Image>
+      <Campaigns></Campaigns>
+      <Whoweare></Whoweare>
+      <Carousel></Carousel>
+      <News></News>
+      <Latestresource></Latestresource>
+      <Contact></Contact>
+    </main>
+  );
 }
