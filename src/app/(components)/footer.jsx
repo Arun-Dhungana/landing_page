690736319c090101b013ded3c5669910da4acfc4
Arun Dhungana <a.d.since03@gmail.com>
2024-06-08 19:22:28 +0545

    roughly completed footer

--- src/app/(components)/footer.jsx
+++ src/app/(components)/footer.jsx
@@ -1,3 +1,19 @@
 export const Footer = () => {
-  return <div >Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer_log">
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>v
+      </div>
+      <div className="footer_bookmark">
+        <h4>Terms and condition</h4>
+        <h4>creative commotions</h4>
+      </div>
+    </footer>
+  );
 };
